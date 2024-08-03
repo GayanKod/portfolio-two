71648d569f7e063706b5c21cc68481d140905f7d
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2024-08-04 03:25:35 +0530

    Update SmtNew.jsx

--- src/components/SmtNew.jsx
+++ src/components/SmtNew.jsx
@@ -7,7 +7,7 @@ export default function SmtNew() {
       <div className="profile">
         <img src="https://cdn.discordapp.com/attachments/945783583413514293/1269383191568056320/1706695963822.png?ex=66afdcb8&is=66ae8b38&hm=2cdd7ca48e91be63775f8c3db68ab2c9dfc473638993911cb72f70c25ff0a5dd&" alt="Profile" className="avatar" />
         <div className="social-links">
-          <a href="https://twitter.com/gayankod" target="_blank" rel="noopener noreferrer"><span></span><span></span><span></span><span></span>Twitter</a>
+          <a href="https://twitter.com/gayankodx" target="_blank" rel="noopener noreferrer"><span></span><span></span><span></span><span></span>Twitter</a>
           <a href="https://linkedin.com/in/gayankod" target="_blank" rel="noopener noreferrer">LinkedIn</a>
           <a href="https://github.com/gayankod" target="_blank" rel="noopener noreferrer">GitHub</a>
         </div>
